0782e1b48ca72e8d0f9883ca6a9eca51d353f3a1
MONE'M <60390230+Ahmed1monm@users.noreply.github.com>
2022-10-22 19:34:56 +0200

    schemas

---
+++ app/database/schemas.py
@@ -0,0 +1,47 @@
+from typing import List
+from pydantic import BaseModel
+
+
+
+class ArticlBase(BaseModel):
+        body: str
+        title: str
+        tags: List[str] = []
+        
+
+
+class ArticleCreate(ArticlBase):
+        auther_id: int
+
+class Article(ArticleCreate):
+        id : int
+
+
+
+class AutherBase(BaseModel):
+        name: str
+        title: str
+        email: str
+        username: str
+        bio: str
+
+class AutherCreate(AutherBase):
+        password: str
+
+class Auther(AutherBase):
+        id: int
+        articles: List[Article] = []
+        class config:
+                orm_mode = True
+
+
+class CommentBase(BaseModel):
+        body: str
+        auther_id: int
+        article_id: int
+
+class CommentCreate(CommentBase):
+        pass
+
+class Comment(CommentBase):
+        id: int

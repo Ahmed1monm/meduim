780e469054e6f1f931af8c3ad819db668ce13e1d
MONE'M <60390230+Ahmed1monm@users.noreply.github.com>
2022-11-27 23:43:22 +0200

    migration and refactor

--- app/migration/env.py
+++ app/migration/env.py
@@ -4,9 +4,9 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from alembic import context
 
-from ....app.database import models
+# from app.database import models
 
-
+from database import models
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
